012df915c0819ebd183b2764705ab3512a1e4a34
pk18763n <34697783+pk18763n@users.noreply.github.com>
2024-04-23 10:03:00 -0500

    Update Macie.py
    Removed unused imports and variables.

--- Macie.py
+++ Macie.py
@@ -1,28 +1,21 @@
 import boto3
 import json
-from inspect import getmembers
-from optparse import OptionParser
-from bson import json_util
-import datetime
+from datetime import datetime
 
-client = boto3.client('macie2',region_name='us-east-1')
-my_list=list()
+client = boto3.client('macie2', region_name='us-east-1')
+finding_ids = list()  # Use a more descriptive name
+
 paginator = client.get_paginator('list_findings')
-index=1
 for finding in paginator.paginate(maxResults=50):
-    for findingid in finding['findingIds']:
-        a=my_list.append(findingid)
-        a=findingid
-        
-for i in my_list:
-    response = client.get_findings(findingIds=[i])
+  finding_ids.extend(finding['findingIds'])  # Use extend for adding multiple elements
+
+def datetime_handler(x):
+  if isinstance(x, datetime):
+    return x.isoformat()
+  raise TypeError("Unknown type")
+
+with open("sample.json", "a") as json_file:
+  for finding_id in finding_ids:
+    response = client.get_findings(findingIds=[finding_id])
     print(response)
-    def datetime_handler(x):
-        if isinstance(x, datetime.datetime):
-            return x.isoformat()
-        raise TypeError("Unknown type")
-    json.dumps(response, default=datetime_handler)
-    with open("sample.json", "a") as json_file:
-        print(response)
-        json.dump(response,json_file,indent=4,default=datetime_handler)
-    
+    json.dump(response, json_file, indent=4, default=datetime_handler)
